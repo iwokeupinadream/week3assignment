425bb58a30852455499c5a2f05e2d5819d7d3724
iwokeupinadream <metzgerdylanjames@gmail.com>
2021-12-16 06:53:37 -0600

    first commit

---
+++ week3code.js
@@ -0,0 +1,118 @@
+// let ages = [3, 9, 23, 64, 2, 8, 28, 93, 108]
+// let sum = 0
+
+// //console.log(ages[ages.length - 1] - ages[0])
+
+// for(let i = 0; i < ages.length; i++){ 
+//     sum = sum + ages[i]
+//   } 
+
+// let averageAge = sum / ages.length
+//   console.log(averageAge)
+
+
+// var names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
+// var sumOfNames = 0;
+//     for(i = 0; i < names.length; i++){
+//         sumOfNames += names[i].length 
+//     }
+// var averageOfNames = sumOfNames / names.length
+// console.log(averageOfNames)
+
+// var names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
+// var everyone = ''
+//     for(i = 0; i < names.length; i++){
+//         if(i < names.length - 1){
+//         everyone = everyone + names[i] +', '
+//     } else everyone = everyone + names[i]
+// }
+//         console.log(everyone)
+
+
+// var names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
+// var nameLengths = []
+// var sumOfNameLengths = 0
+
+//     for(i = 0; i < names.length; i++){
+//         nameLengths.push(names[i].length)
+//     }
+
+//     for(i = 0; i < nameLengths.length; i++){
+//         sumOfNameLengths = sumOfNameLengths + nameLengths[i]
+//     }
+//     console.log(sumOfNameLengths)
+
+    //  function myFunction(word, n){
+    //      return word.repeat(n)
+    //  }console.log(myFunction("Hello", 3))
+
+    // function myFunction2(firstName, lastName){
+    //     return firstName + ' ' + lastName
+    // }
+    //      console.log(myFunction2('Dylan', 'Metzger'))
+
+    // let arrayOfNumbers = [3, 9, 23, 64, 2, 8, 28, 93, 108]
+    // let sum = 0
+
+    // function myFunction3(array){
+    //     for(i = 0; i < array.length; i++){
+    //         sum += array[i]
+    //     }
+    //     if(sum > 100){
+    //         return true
+    //     }else return false 
+    
+    // } console.log(myFunction3(arrayOfNumbers))
+
+    // let arrayOfNumbers = [3, 9, 23, 64, 2, 8, 28, 93, 108]
+    // let sum = 0
+    // var average
+
+    // function myFunction4(array){
+    //     for(i = 0; i < array.length; i++){
+    //         sum += array[i]
+    //     }
+    //     average = sum / array.length
+    //     return average
+    // } console.log(myFunction4(arrayOfNumbers))
+
+    // let arrayOfNumbers = [3, 9, 23, 64, 2, 8, 28, 93, 108]
+    // let arrayOfNumbers2 = [4, 10, 19, 34, 8, 16, 63, 71, 99]
+    
+    
+    // function myFunction5(array1, array2){
+    //     let sum1 = 0
+    //     let sum2 = 0
+        
+    //     for(i = 0; i < array1.length; i++){
+    //         sum1 += array1[i]
+    //     }
+    //     for(i = 0; i < array2.length; i++){
+    //         sum2 += array2[i]
+    //     }
+        
+    //     let average1 = sum1 / array1.length
+    //     let average2 = sum2 / array2.length
+
+    //     if(sum1 > sum2){
+    //         return true
+    //     } else if(sum1 < sum2){
+    //         return false
+    //     }
+    // }
+
+    // console.log(myFunction5(arrayOfNumbers, arrayOfNumbers2))
+
+    // function willBuyDrink(isHotOutside, moneyInPocket){
+    //     if(isHotOutside == true && moneyInPocket > 10.50){
+    //         return true
+    //     } else return false
+    // } 
+    
+    // console.log(willBuyDrink(true, 10.51))
+    // console.log(willBuyDrink(false, 10.51))
+    // console.log(willBuyDrink(true, 10.49))
+
+    // function myOwnFunction(name, dayOrNight){
+    //     return "welcome " + name + " i hope you have a wonderful " + dayOrNight
+    //     } console.log(myOwnFunction("Dylan", "Night"))
